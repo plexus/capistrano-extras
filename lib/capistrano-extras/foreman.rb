37074b94aa82f12b6ea19e893b80216ce4987bc9
Arne Brasseur <arne@arnebrasseur.net>
2012-05-28 19:32:37 +0200

    added capistrano config block

--- lib/capistrano-extras/foreman.rb
+++ lib/capistrano-extras/foreman.rb
@@ -2,33 +2,34 @@
 #
 # Foreman tasks
 
-namespace :foreman do
-  desc 'Export the Procfile to Ubuntu upstart scripts'
-  task :export, :roles => :app do
+Capistrano::Configuration.instance(true).load do
+  namespace :foreman do
+    desc 'Export the Procfile to Ubuntu upstart scripts'
+    task :export, :roles => :app do
+      
+      run "cd #{release_path} && sudo bundle exec foreman export upstart /etc/init -a #{application} -u #{user} -l #{release_path}/log/foreman"
 
-    run "cd #{release_path} && sudo bundle exec foreman export upstart /etc/init -a #{application} -u #{user} -l #{release_path}/log/foreman"
+    end
+    
+    desc "Start the application services"
+    task :start, :roles => :app do
+      
+      sudo "start #{application}"
+    end
 
+    desc "Stop the application services"
+    
+    task :stop, :roles => :app do
+      sudo "stop #{application}"
+    end
+
+    desc "Restart the application services"
+    task :restart, :roles => :app do
+      
+      run "sudo start #{application} || sudo restart #{application}"
+    end
   end
-
-  desc "Start the application services"
-  task :start, :roles => :app do
-
-    sudo "start #{application}"
-  end
-
-  desc "Stop the application services"
-
-  task :stop, :roles => :app do
-    sudo "stop #{application}"
-
-  end
-
-  desc "Restart the application services"
-  task :restart, :roles => :app do
-
-    run "sudo start #{application} || sudo restart #{application}"
-  end
+  
+  after "deploy:update", "foreman:export"    # Export foreman scripts
+  after "deploy:update", "foreman:restart"   # Restart application scripts
 end
-
-after "deploy:update", "foreman:export"    # Export foreman scripts
-after "deploy:update", "foreman:restart"   # Restart application scripts
